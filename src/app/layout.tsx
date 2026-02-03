9a7beeb5adefa2e463e904e64e77fac26554493e
mert çalışkan <kasirgatv966@gmail.com>
2026-02-03 20:03:04 +0300

    Replace GTM with Google Analytics 4 (gtag)
    - Remove Google Tag Manager implementation
    - Add Google Analytics 4 with ID G-XXL7KKBSB0
    - Use Next.js Script component with afterInteractive strategy
    - Cleaner implementation with direct GA4 tracking
    - Bundle size: 179kB maintained

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -143,31 +143,23 @@ export default function RootLayout({
                 }} />
             </head>
             <body className={`${inter.className} antialiased`}>
-                {/* Google Tag Manager Script */}
+                {/* Google Analytics */}
                 <Script
-                    id="gtm-script"
+                    src="https://www.googletagmanager.com/gtag/js?id=G-XXL7KKBSB0"
+                    strategy="afterInteractive"
+                />
+                <Script
+                    id="google-analytics"
                     strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
                         __html: `
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-PD6ZJTVD');
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-XXL7KKBSB0');
                         `
                     }}
                 />
-
-                {/* Google Tag Manager (noscript) */}
-                <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-PD6ZJTVD"
-                        height="0"
-                        width="0"
-                        style={{ display: 'none', visibility: 'hidden' }}
-                    />
-                </noscript>
-                {/* End Google Tag Manager (noscript) */}
 
                 <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
                     {children}
